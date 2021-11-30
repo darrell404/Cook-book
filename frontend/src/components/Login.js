0da5508ca72dd2f5c35bdfae4499878eb10236f0
darrell404 <darrell029@gmail.com>
2021-11-30 10:57:29 +0000

    Working on checking if passwords match on Register page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -22,10 +22,10 @@ export default class Login extends Component {
             <div className="w-100 h-100 n-0 p-0">
                 <div id="login-container" className="d-flex w-100 h-75">
                     <form id="login-form" className="d-flex flex-column w-25 h-25 align-items-center text-center justify-content-center" onSubmit={this.handleSubmit}>
-                        <input type="text" id="name" className="text-input border-secondary mb-1 border-2 form-control" name="name" placeholder="Email address" required></input>
+                        <input type="email" id="email" className="text-input border-secondary mb-1 border-2 form-control" name="email" placeholder="Email address" required></input>
                         <input type="password" id="password" className="text-input border-secondary mt-1 border-2 form-control" name="password" placeholder="Password" required></input>
                         <input id="submit" className="m-4 btn-sm btn-secondary" type="submit" value="Login" />
-                        <p>If you do not have an account yet, <Link to='/register'><button className="btn btn-primary">register here</button></Link></p>
+                        <p>If you do not have an account yet, click <Link to='/register'> here </Link> to register</p>
                     </form>
                 </div>
             </div>
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -5,6 +5,12 @@ import '../css/main.css'
 export default class Register extends Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            password: '',
+            confirmpassword: ''
+        }
+
         this.handleSubmit = this.handleSubmit.bind(this)
         this.onChange = this.onChange.bind(this)
     }
@@ -14,6 +20,10 @@ export default class Register extends Component {
     }
 
     onChange(event) {
+        this.setState({password: event.target.value})
+        if (this.state.password.match(this.state.confirmpassword)) {
+            console.log("Passwords match")
+        }
     }
 
     render() {
@@ -21,10 +31,11 @@ export default class Register extends Component {
             <div className="w-100 h-100 n-0 p-0">
                 <div id="login-container" className="d-flex w-100 h-75">
                     <form id="login-form" className="d-flex flex-column w-25 h-25 align-items-center text-center justify-content-center" onSubmit={this.handleSubmit}>
-                        <input type="text" id="name" className="text-input border-secondary mb-1 border-2 form-control" name="name" placeholder="Email address" required></input>
-                        <input onChange={this.onChange} type="password" id="password" className="text-input border-secondary mt-1 border-2 form-control" name="password" placeholder="Password" required></input>
-                        <input onChange={this.onChange} type="password" id="password" className="text-input border-secondary mt-1 border-2 form-control" name="password-retype" placeholder="Re-type Password" required></input>
+                        <input type="email" id="email" className="text-input border-secondary mb-1 border-2 form-control" name="email" placeholder="Email address" required></input>
+                        <input type="password" id="password" className="text-input border-secondary mt-1 mb-1 border-2 form-control" name="password" placeholder="Password" required></input>
+                        <input onChange={this.onChange} type="password" id="password" className="text-input border-secondary mt-1 border-2 form-control" name="password-retype" placeholder="Confirm Password" required></input>
                         <input id="submit" className="m-4 btn-sm btn-secondary" type="submit" value="Create Account" />
+                        
                     </form>
                 </div>
             </div>
